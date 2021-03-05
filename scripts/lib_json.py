639f82ba7e2f165f5fe2bdac853341cd954efcf1
FedericoAngelini <39492138+FedericoAngelini@users.noreply.github.com>
2021-03-05 12:31:37 +0000

    Added: lib_json.py, vars.py, .gitignore

---
+++ scripts/lib_json.py
@@ -0,0 +1,96 @@
+import json
+import numpy as np
+from scripts.vars import *
+import os
+from matplotlib import collections as mc
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import pickle
+
+
+def read(file):
+    with open(file) as json_file:
+        data = np.array(json.load(json_file)['people'][0]['pose_keypoints_2d'])
+        if isinstance(data, list):
+            return np.nan * np.zeros((15, 3))
+        else:
+            data[data == 0] = np.nan
+            x = np.zeros((25, 3))
+            for i in range(0, 3):
+                x[:, i] = data[range(i, 75, 3)]
+            x[0, :] = np.nanmean(x[openpose_parts['head'], :], axis=0)
+            x[11, :] = np.nanmean(x[openpose_parts['right_foot'], :], axis=0)
+            x[14, :] = np.nanmean(x[openpose_parts['left_foot'], :], axis=0)
+            return x[0:15, :]
+
+
+def create_limbs(body):
+    graph = [[0, 1], [1, 2], [1, 5], [1, 8], [2, 3], [3, 4],
+             [5, 6], [6, 7], [8, 9], [8, 12], [9, 10], [10, 11],
+             [12, 13], [13, 14]]
+    limbs = []
+    for i in graph:
+        limbs.append([(body[i[0], 0], body[i[0], 1]), (body[i[1], 0], body[i[1], 1])])
+    return limbs
+
+
+def animation(seq, path, filename):
+
+    def animationUpdate(k):
+        x = seq[:, 0, k]
+        y = seq[:, 1, k]
+        scat.set_offsets(np.c_[x, y])
+        lines = create_limbs(seq[:, :, k])
+        lc = mc.LineCollection(lines, linewidths=2, alpha=0.1)
+        ax1.add_collection(lc)
+        return scat,
+
+    f = plt.figure(figsize=(10, 5))
+    min_x = seq[:, 0, :].min().round()
+    max_x = seq[:, 0, :].max().round()
+    width = max_x - min_x
+    min_y = seq[:, 1, :].min().round()
+    max_y = seq[:, 1, :].max().round()
+    heigth = max_y - min_y
+    xlim = (min_x - width*0.2, max_x + width*0.2)
+    ylim = (min_y - heigth*0.2, max_y + heigth*0.2)
+    ax1 = plt.subplot(111, xlim=xlim, ylim=ylim)
+    scat = ax1.scatter(x=seq[:, 0, 0], y=seq[:, 1, 0], s=70)
+    lines = create_limbs(seq[:, :, 0])
+    lc = mc.LineCollection(lines, linewidths=2, alpha=0.1)
+    ax1.add_collection(lc)
+    anim = FuncAnimation(f, animationUpdate, frames=seq.shape[2], interval=40, blit=True)
+    plt.gca().invert_yaxis()
+    plt.title(filename)
+    plt.axis('off')
+    plt.gca().set_aspect('equal', adjustable='box')
+    anim.save(os.path.join(path, filename) + '.gif', writer='pillow')
+    print('Saved GIF: {}'.format(os.path.join(path, filename) + '.gif'))
+
+
+def read_sequence(sample, animate_path=None):
+    jsons = os.listdir(sample)
+    sequence = np.nan * np.zeros((15, 3, len(jsons)))
+    for i in jsons:
+        t = int(i[-27:-15])
+        sequence[:, :, t] = read(os.path.join(sample, i))
+    if animate_path is not None:
+        animation(sequence, animate_path, os.path.basename(sample).replace('.avi', ''))
+
+    return sequence
+
+
+def save_sequence(seq, sample, path=seq_path):
+    res = [i for i in range(len(sample)) if sample.startswith('_', i)]
+    to_save = dict(seq=seq,
+                   sample=sample,
+                   dataset=sample[:res[0]],
+                   actor=sample[res[0]+1:res[1]],
+                   unique_id=sample[res[1]+1:res[2]],
+                   length=seq.shape[2])
+    pickle.dump(to_save, open(os.path.join(path, sample+'.p'), 'wb'))
+
+
+sample = '../data/json/i3dpost_chris_bend_177.avi'
+seq = read_sequence(sample)
+save_sequence(seq, os.path.basename(sample))
