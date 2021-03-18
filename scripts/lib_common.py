c05aa6f305bbde46ae7f87b4546076068c8a3721
FedericoAngelini <federico.angelini@gmx.com>
2021-03-18 11:26:19 +0000

    updated lib_common.py

--- scripts/lib_common.py
+++ scripts/lib_common.py
@@ -19,19 +19,15 @@ import numpy as np
 
 def read_poses(path=paths['pose']):
     report = pd.DataFrame(columns=['sample', 'dataset', 'lenght', 'aver_conf'])
-    data = dict()
-    frames = dict()
     for i in os.listdir(path):
         with open(os.path.join(path, i), 'rb') as f:
             d = pickle.load(f)
-        report = report.append({'sample': d['sample'],
+        report = report.append({'sample': d['name'],
                                 'dataset': d['dataset'],
                                 'actor': d['actor'],
                                 'action': d['action'],
                                 'length': d['length'],
                                 'aver_conf': np.nanmean(d['seq'][:, 2, :])}, ignore_index=True)
-        data[i] = d['seq']
-        frames[i] = d['frames']
 
-    return report, data, frames
+    return report
 
